b95996059e5f4958605c76ba4a24edeff74ba142
YogeneP <base@yogene.ru>
2025-12-10 16:06:42 +0300

    G20 complete

--- HW10/G19_triangles.c
+++ HW10/G19_triangles.c
@@ -1,76 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <limits.h>
 
-void build_triangle(uint16_t c);
+void build_triangle(uint16_t n, FILE** f);
+int open_file(const char* filename, FILE** f, const char* mode);
 
 int main() {
-	build_triangle(10);
-/*
 	FILE* fi;
+	FILE* fo;	
 	 
-	if( !(fi = fopen("input.txt", "rt")) ) {
-		printf("Cant't open file");
-		return 1;
-	}
-	
+	if ( open_file("input.txt", &fi, "rt") ) return -1;
+	if ( open_file("output.txt", &fo, "wt")) return 1;	
+
 	uint16_t count = 0;
+	char c = 0;
 	
 	while (((c = fgetc(fi)) != EOF)) {
 		if (c == '*') count++;
 	}
-	
+
+	fclose(fi);
+
 	if (count >= 3) {
 
-		uint64_t f[2] = {1, 2};
-		uint16_t n = 2;
-		
+//		uint64_t f[2] = {1, 2};
+		uint64_t b = 1;
+		uint16_t num = 1;
+				
 		for (uint16_t i=0; i < UINT_MAX; i++) {
-			f[b] = f[0] + f[1];
+			b += num + 1;
 			num++;
-			printf(" %" PRIu64"\n", f[b]);
-			if (f[b] > count) break;
-			if (f[b] = count) { 
-				build_triangle(num);
+//			printf("%d: %lld\n", num, f[b]);
+			if (b > count) break;
+			if (b == count) { 
+				build_triangle(num, &fo);
 				return 0;
 			}
-			b = !b;
 		}
 	} 
+//	printf("NO");
+	fprintf(fo, "NO");
 	
-	printf("NO");
+	fclose(fo);
+	
 	return 0;
-	
-		printf("%c; code = %d\n", c, code = get_letter_code(&c));
-		if( ((code = get_letter_code(&c)) > 0) && (code + '0' != str[i-1]) ) {
-			str[i] = code + '0';
-			i++;
-		}
-	}
-*/ }
+}
 
-void build_triangle(uint16_t n) {
-	FILE* fo;
-	
-	if( !(fo = fopen("output.txt", "wt")) ) {
-		printf("Cant't open file");
-		return;
-	}
+
+int open_file( const char *filename, FILE **fptr, const char *mode) {
+    FILE *fp = fopen(filename, mode);
+    if (fp == NULL) {
+        printf("Can't open file: %s\n", filename);
+        return 1;
+    }
+    *fptr = fp;
+    return 0;
+}
+
+void build_triangle(uint16_t n, FILE** f) {
+	if (*f == NULL) return;
+	 
 	uint16_t s_pos = n-1;
 	char c[2] = {' ', '*'};
 	uint8_t ci = 0;
 	for(uint16_t i = 0; i < n; i++) {
-		ci = 0;
+		ci = 0; 
 		for(uint16_t j = 0; j < n*2; j++) {
 			if (j >= s_pos) { 
-				if (j > n + i) break;
+				if (j > n + i - 1) break;
 				ci = !ci;
 			}
-			printf("%c",c[ci]);
-			fputc(c[ci],fo); 
+//			printf("%c",c[ci]);
+			fputc(c[ci],*f); 
 		}
-		printf("\n");
-		fputc('\n',fo);
+//		printf("\n");
+		fputc('\n',*f);
 		s_pos--;
 	}
 }
